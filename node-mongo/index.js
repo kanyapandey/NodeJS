83af27308d4490eef9caf7b8d47fc0c1527149ce
kanyapandey <kanyapandey2603@gmail.com>
2019-06-24 17:54:07 +0700

    Node Mongoose Part 1

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,29 +1,30 @@
 const MongoClient = require('mongodb').MongoClient
 const assert = require('assert')
-
+const dboper = require('./operations')
 const url = 'mongodb://localhost:27017/'
 const dbname = 'conFusion'
 
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err,null)
+MongoClient.connect(url).then((client) => {
+    // assert.equal(err,null)
     console.log('Connected correctly to server')
     const db = client.db(dbname)
-    const collection = db.collection('dishes')
-    collection.insertOne({"name":"kanya","description":"test"}, (err,result) => {
-        assert.equal(err,null)
-        console.log('After insert \n')
-        console.log(result.ops)
-
-        collection.find({}).toArray((err,docs) => {
-            assert.equal(err,null)
-            console.log('Found:\n')
-            console.log(docs)
-
-            db.dropCollection('dishes', (err,result) => {
-                assert.equal(err,null)
-
-                client.close()
-            })
-        })
-    })
-})
+    
+    dboper.insertDocument(db, {name:"kenika",description:"thai"}, 'dishes').then((result) =>{
+            // ops: no of operations we've carried out
+            console.log('Inserted document:\n', result.ops)
+            return dboper.findDocument(db, 'dishes')
+        }).then((docs) => {
+            console.log('Found documents:\n', docs)
+            return dboper.updateDocument(db, {name: "kenika"}, {description: "thai nichi"}, 'dishes')
+        }).then((result) => {
+            console.log('Updated documents:\n', result.result)
+            return dboper.findDocument(db, 'dishes')
+        }).then((docs) => {
+            console.log('Found documents:\n', docs)
+            return db.dropCollection('dishes')
+        }).then((result) => {
+            console.log('Dropped Collection:\n', result)
+            client.close()
+        }).catch((err) => console.log(err));
+    
+}).catch((err) => console.log(err));
---
+++ node-mongoose/index.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Dishes = require('./models/dishes')
+const url = 'mongodb://localhost:27017/conFusion'
+const connect = mongoose.connect(url)
+
+connect.then((db) => {
+    console.log('Connected to server')
+
+    var newDish = Dishes({
+        name: "shilpa",
+        description: "spie"
+    })
+
+    newDish.save().then((dish) => {
+        console.log(dish)
+        return Dishes.find({}).exec()
+    }).then((dishes) => {
+        console.log(dishes)
+        return Dishes.remove({})
+    }).then(() => {
+        return mongoose.connection.close()
+    }).catch((err) => {
+        console.log(err)
+    })
+})
